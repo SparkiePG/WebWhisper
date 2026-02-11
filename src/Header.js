15dab59f81008c0473cbea0dfb78cea82db2d49b
PG <74737305+SparkiePG@users.noreply.github.com>
2026-02-11 21:48:14 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+ <header className="bg-gray-800 text-white p-4">
+ <h1 className="-2xl font-bold">WebWhisper</h1>
+ </header>
+);
+
+export default Header;
