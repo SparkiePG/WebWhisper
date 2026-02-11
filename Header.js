259eddd3e5e290875694d5ba8b880655e8751def
PG <74737305+SparkiePG@users.noreply.github.com>
2026-02-11 21:46:43 +0530

    Delete Header.js

--- Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
- <header className="bg-gray-800 text-white p-4">
- <h1 className="-2xl font-bold">WebWhisper</h1>
- </header>
-);
-
-export default Header;
